c4de0e897510f4134ef9e11540481f3803455a27
Shobhit Mudgal <137764716+8sho@users.noreply.github.com>
2026-06-23 16:29:31 +0530

    Update init.database.sql

--- script/init.database.sql
+++ script/init.database.sql
@@ -1,5 +1,11 @@
--- PROJECT DATA WAREHOUSE --
--- CREATE A NEW DATABASE --
+/*SCRIPT PURPOSE -
+this script creates a new database named 'data warehouse' after checking ifit already exists , if the database exists , it is dropped and recreated.
+additionally the script sets up three schemas within the datasbase , bronze , silver . gold */
+/* warning -
+running this script will drop the entire 'data warehouse' database if it exists / all data in the database will be permanently deleted ,
+proceed with caution and ensure you have proper backups before running this script */
+
+
 
 create database DataWarehouse;
 
