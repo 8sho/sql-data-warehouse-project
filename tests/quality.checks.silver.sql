f7a22de570b5223b2413fe7600669b19becb2504
Shobhit Mudgal <137764716+8sho@users.noreply.github.com>
2026-08-02 21:39:38 +0530

    Update quality.checks.silver.sql

--- tests/quality.checks.silver.sql
+++ tests/quality.checks.silver.sql
@@ -43,7 +43,7 @@
  OR sls_ship_date > 20500101
  OR sls_ship_date < 19000101
 
-
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
      select *
 from bronze.crm_cust_info
 
@@ -94,6 +94,46 @@ from bronze.crm_cust_info -- nothing is wrong here but it would be good if there
 						  
 
   -- check for invalid date orders
+		 
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ -- now to check for duplicate primary key
+
+select
+prd_id,
+COUNT(*)
+from bronze.crm_prd_info
+group by prd_id
+having COUNT(*) > 1                                       -- no dupliactes found , means no need to do anything in id column 
+
+
+		 select
+prd_id,
+REPLACE(SUBSTRING(prd_key , 1 , 5), '-' , '_') as cat_id, -- we split the cat id in 2 parts to join it with the table having the same columns and replacing to match with the other column
+SUBSTRING(prd_key , 7 , LEN(prd_key)) as prd_key,         -- extract product key
+prd_nm,                                                   -- checked for unwanted spaces and there weren't any 
+ISNULL(prd_cost, 0) as prd_cost,                          -- to replace nulls with 0 we can use coalesce as well
+CASE UPPER(TRIM(prd_line))                                -- correcting names for standardization
+     WHEN 'R' THEN 'road'                                 -- another way of simple mapping 
+	 WHEN 'M' THEN 'mountain'
+     WHEN 'S' THEN 'other sales'
+	 WHEN 'T' THEN 'touring'
+     ELSE 'n/a'
+END as prd_line,
+CAST(prd_start_date AS DATE) AS prd_start_date,  -- cast to change the datatype
+CAST(LEAD(prd_start_date) OVER (PARTITION BY prd_key order by prd_start_date)-1 AS DATE) AS prd_end_date -- dates changed based on the next entry 
+from bronze.crm_prd_info
+
+select
+*
+from bronze.crm_prd_info
+where prd_start_date > prd_end_date /* logic says that the end date should be higher than start date 
+                                       but in here there are several dates that does not follow the rule and mismatched 
+									   so we will use the start date of the next product as the end date of the previous one 
+									   and will do this for all the mismatched dates */
+									/* before transferring the data to silver table we have to keep in mind the changes that we made ,
+									   that are not present in the normal table so we have to make the necessary changes */
+----------------------------------------------------------------------------------------------------------------------------------------------------------------		 
 
   select *
   from bronze.crm_sales_details
@@ -122,6 +162,8 @@ from bronze.crm_cust_info -- nothing is wrong here but it would be good if there
   where sls_sales != sls_quantity * sls_price
   OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
   OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------		 
 
  select
   cst_id,
@@ -173,7 +215,8 @@ select
 	   ELSE 'n/a'
   END cst_gender
   from bronze.erp_cust_az12   -- issue fixed
-
+		 
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
   select
   *
   from
@@ -203,6 +246,8 @@ CASE WHEN TRIM(cst_country) = 'DE' THEN 'GERMANY'
 	 ELSE TRIM(cst_country)
 END as cst_country
 from bronze.erp_loc_a101  -- resolved the issue 
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------		 
 
 select *
 from bronze.erp_px_cat_g1v2 -- in this the first column has a relation with silver.crm_prd_info
